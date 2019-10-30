ff5357fc6cdab36c4338be0ad90323091810ff13
Josh Paul <paul22@ad.nmc.edu>
2019-10-30 10:24:59 -0400

    Add project files.

---
+++ HelloNMC/Program.cs
@@ -0,0 +1,16 @@
+﻿dusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello NMC");
+        }
+    }
+}
